15f84d41bc656a765c91c9d5d409e283404a5ec6
Diego Cicotoste <cicotosted@gmail.com>
2024-11-19 10:51:10 -0500

    pushing it to pages
    Co-authored-by: yurahriaziev <yura.hriaziev@gmail.com>

--- backend/config.py
+++ backend/config.py
@@ -1,16 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_session import Session
+import os
 
 app = Flask(__name__)
-app.secret_key = 'a2ea490452f07b815f2ee39eaab6d1fc519cc2144d34d312535a27a20109e955'
+app.secret_key = "a2ea490452f07b815f2ee39eaab6d1fc519cc2144d34d312535a27a20109e955"
 
-# app.config["SESSION_TYPE"] = "filesystem"  # Store sessions on the file system
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_USE_SIGNER"] = True
-# Session(app)
-
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 
 PREDEFINED_TICKERS = [
